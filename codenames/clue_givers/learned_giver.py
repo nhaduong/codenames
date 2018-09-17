da91c65fcbccb1026a6f4245611e49908d4e4174
nhaduong <qu@cs.washington.edu>
2018-09-17 11:14:43 -0700

    wip embedding transformation

--- codenames/clue_givers/learned_giver.py
+++ codenames/clue_givers/learned_giver.py
@@ -1,9 +1,8 @@
-# from codenames.clue_givers.giver import Giver
-import logging
 import operator
 from itertools import combinations, chain, permutations
 from typing import List
 import time
+from overrides import overrides
 
 import codenames.utils.game_utils as util
 import numpy as np
@@ -17,17 +16,32 @@ from codenames.embedding_handler import EmbeddingHandler
 from codenames.utils.game_utils import Clue, DEFAULT_NUM_CLUES, UNREVEALED, GOOD, BAD, CIVILIAN, ASSASSIN, DEFAULT_NUM_TARGETS, CIVILIAN_PENALTY, ASSASSIN_PENALTY, MULTIGROUP_PENALTY, DEPTH, DEFAULT_NUM_GUESSES
 
 
-class HeuristicGiver(Giver):
+LEARNING_RATE = .005
 
+import numpy as np
+
+class LearnedGiver(Giver):
     def __init__(self,
                  embedding_handler: EmbeddingHandler,
                  guesser_embedding_handler: EmbeddingHandler,
                  blacklist: List[str] = [],
-                 current_board: List[str] = None,):
+                 current_board: List[str] = None,
+                 train: bool=False) -> None:
+
         self.embedding_handler = embedding_handler
         self.guesser_embedding_handler = guesser_embedding_handler
         self.blacklist = blacklist
         self.current_board = current_board
+
+        self.train = train
+        self.projection = np.add(np.random.normal(0,.1,embedding_handler.embedding_weights.shape[0]),np.identity(embedding_handler.embedding_weights.shape[0])) #initialize identity matrix with some noise
+        self.projection_history = []
+        self.projection_log_probs = []
+
+    def _update_embeddings(self,
+                           embedding_handler: EmbeddingHandler,
+                           projection: List[float]) -> EmbeddingHandler:
+        return np.matmul(projection, embedding_handler.embedding_weights)
 
     def _check_illegal(self,
                        clue_word: str,
@@ -35,8 +49,25 @@ class HeuristicGiver(Giver):
         for word in illegal:
             if word in clue_word:
                 return True
+            else:
+                return False
+    def sim2prob(self,
+                 clue_word: str,
+                 board: List[str],
+                 embedding_handler: EmbeddingHandler) -> List[float]:
+        '''Converts 1 - similarity scores into probabilities. Compute on 1-sim because we want the lowest similarity to have the highest distance.
+            Converts by normalizing inverse distances with: 1/dist / sum(1/dist). Could possibly try using squares or higher order inverses to skew probability
+            distribution more. Higher skew might make it easier to learn?
+            outputs shape (len(board), 1, 1)
+        '''
 
-        return False
+        clue_neg_sims = np.array([1-cosine_similarity(embedding_handler.get_word_vector(board_word).reshape(1,-1), embedding_handler.get_word_vector(clue_word).reshape(1,-1)) for board_word in board])
+        # print(clue_neg_sims)
+        # return self.stable_softmax(clue_neg_sims)
+        sim_sum = np.sum(1/clue_neg_sims)
+        clue_neg_probs = [1/sim / sim_sum for sim in clue_neg_sims]
+        #assert np.sum(clue_neg_probs) == 1, 'Distances converted to probabilities don\'t add up to 1!, {}{}'.format(np.sum(clue_neg_probs), clue_neg_probs)
+        return clue_neg_probs
 
     ''' Returns list of n=NUM_CLUES of Clues for a given group of words'''
     def _get_clues(self, allpos, pos_words_subset, neg_words, civ_words, ass_words, available_illegal, aggressive, random_clues=False, num_clues=DEFAULT_NUM_CLUES,MULTIGROUP_PENALTY=MULTIGROUP_PENALTY):
@@ -54,7 +85,6 @@ class HeuristicGiver(Giver):
                    'padlock','seaport','swung','racehorse','choco','upfield','cyclic',
                    'racetrack','beaten','agency','german','french','novella','novelist','novelette',
                    'dino','forearm','underpants']
-
         pos_words_vectors = self.embedding_handler.embed_words_list(pos_words_subset)
         neg_words_vectors = self.embedding_handler.embed_words_list(neg_words)
         civ_words_vectors = self.embedding_handler.embed_words_list(civ_words)
@@ -152,15 +182,56 @@ class HeuristicGiver(Giver):
             yield item
             last = item[0]
 
+    def stable_softmax(self, X): #outputs array that almost adds up to 1
+        exps = np.exp(X - np.max(X))
+        return exps / np.sum(exps)
+
+    def _skipgram(self,
+                  board: List[str],
+                  clue: tuple,
+                  game_state: List[int]):
+        '''returns updated projection of original embedding_handler and loss'''
+        giver_truth = [1] * len(board) #predicted
+        guesser_truth = [1] * len(board) #real truth
+
+        for word in clue.intended_board_words:
+            idx = board.index(word)
+            giver_truth[idx] = 0
+        guesses = self._guess(board, clue.clue_word, -2, game_state)
+        for word in guesses[:3]:
+            idx = board.index(word)
+            guesser_truth[idx] = 0
+
+        #softmax the sim scores from clueword to board words
+        giver_probs = self.sim2prob(clue.clue_word, board, self.embedding_handler)  # ? or keep it as (25,1,1)?
+        #guesser_probs = self.sim2prob(clue.clue_word, board, self.guesser_embedding_handler)
+
+        loss = 0
+        for y, p in zip(guesser_truth, giver_probs):
+            loss += -(y * np.log(p) + (1 - y) * np.log(1 - p))
+
+        error = np.array([-label + g for label, g in zip(guesser_truth, self.stable_softmax(giver_probs)) ])
+        dW2 = np.outer(self.projection, np.sum(error, axis=0))
+        self.projection -= (LEARNING_RATE * dW2).reshape(self.projection.shape)
+        new_embeddings = self._update_embeddings(self.embedding_handler, self.projection)
+
+        self.projection_history.append(self.projection)
+        return new_embeddings, loss
+
+
     def get_next_clue(self,
                       board: List[str],
                       allIDs: List[int],
                       game_state: List[int],
-                      score: int,):
+                      score: int,
+                      previous_guess: List[str] = None,
+                      ) -> List[Clue]:
+        '''Creates empty blacklist upon each new game'''
         if self.current_board != board:
-            self.blacklist = ['dentin']
+            self.blacklist = []
             self.current_board = board
-        start = time.time()
+
+
         pos_words = [board[idx] for idx, val in enumerate(allIDs) if val == GOOD]
         neg_words = [board[idx] for idx, val in enumerate(allIDs) if val == BAD]
         civ_words = [board[idx] for idx, val in enumerate(allIDs) if val == CIVILIAN]
@@ -211,6 +282,7 @@ class HeuristicGiver(Giver):
         clues_by_group.sort(key=operator.itemgetter(1),reverse=True)
         clues_by_group = [clue[0] for clue in clues_by_group]
 
+        #filter out clues    TODO: make a filter function
         filtered_clues_by_group = [clue for clue in clues_by_group if clue.clue_word not in self.blacklist]
         if filtered_clues_by_group:
             self.blacklist.append(filtered_clues_by_group[0].clue_word)
@@ -218,7 +290,7 @@ class HeuristicGiver(Giver):
             self.blacklist.append(filtered_clues_by_group[0])
             filtered_clues_by_group.pop(0)
 
-        #safety measure for if no good clue can be generated
+        #safety measure for if no good clue can be generated; just gets top N clues that are closest to pos words, ignoring negative words
         if len(filtered_clues_by_group) == 0:
             clues_by_group = []
             for count in range(len(available_targets), 0, -1):
@@ -237,13 +309,20 @@ class HeuristicGiver(Giver):
             self.blacklist.append(filtered_clues_by_group[0])
             filtered_clues_by_group.pop(0)
 
+        clue = filtered_clues_by_group[0]
+        print(self.embedding_handler)
+        #update the embedding handler for the next turn
+        self.embedding_handler.vocab, loss = self._skipgram(board, clue, game_state)
+        print("loss: {}".format(loss))
+        #print(self.projection_history)
+        #print(self.embedding_handler)
         return filtered_clues_by_group
 
 def main():
-    embed = '/home/nikita/Downloads/codenames/data/googlenews_SLIM.txt'
+    embed = '/home/nikita/Downloads/codenames/data/uk_embeddings.txt'
     test_embed = EmbeddingHandler(embed)
-    hg = HeuristicGiver(test_embed, EmbeddingHandler('/home/nikita/Downloads/codenames/data/glove.6B.300d.word2vec.txt'))
-    board = ['witch','cap','novel','bear','tooth','horseshoe','scientist','ham','spell','bark','dragon','embassy','row','club','book','fire','pole','green','force','whip','life','canada','tag','nail','mouse']
+    hg = LearnedGiver(test_embed, EmbeddingHandler('/home/nikita/Downloads/codenames/data/uk_embeddings.txt'))
+    board = ['witch','cap','novel','bear','tooth','tennis','scientist','ham','spell','bark','dragon','embassy','row','club','book','fire','pole','green','force','whip','life','canada','tag','nail','mouse']
     allIDs = [ASSASSIN,GOOD,GOOD,GOOD,GOOD,GOOD,GOOD,GOOD,GOOD,GOOD,BAD,BAD,BAD,BAD,BAD,BAD,BAD,BAD,CIVILIAN,CIVILIAN,CIVILIAN,CIVILIAN,CIVILIAN,CIVILIAN,CIVILIAN]
     game_state = [UNREVEALED, UNREVEALED, UNREVEALED, UNREVEALED, 2, UNREVEALED, UNREVEALED, 2,UNREVEALED,UNREVEALED,UNREVEALED,2,2,UNREVEALED,UNREVEALED,UNREVEALED,UNREVEALED,UNREVEALED,UNREVEALED,2,UNREVEALED,UNREVEALED,UNREVEALED,2,UNREVEALED]
     score = 3
